65caf2defbabbe86291ae7787911e303b143fe6f
sumandev07 <sumanpreet15307@gmail.com>
2026-07-28 12:57:35 +0530

    exp 4.1

--- Experiment-4/4.1/code.sql
+++ Experiment-4/4.1/code.sql
@@ -1,2 +1,14 @@
 
 -- link -> https://www.codechef.com/learn/course/sql-intermediate/SQ00BS01/problems/ASQL01D?tab=statemAent
+
+SELECT c.customer_name, o.order_date
+FROM customers c
+INNER JOIN orders o ON c.customer_id = o.customer_id;
+
+SELECT c.customer_name, o.product_name
+FROM customers c
+LEFT JOIN orders o ON c.customer_id = o.customer_id;
+
+SELECT p.product_name, o.order_date
+FROM products p
+INNER JOIN orders o ON p.product_name = o.product_name;
